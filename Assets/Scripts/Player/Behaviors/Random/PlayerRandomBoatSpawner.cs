7f4e108d3be5c4537ac3bd5457fa30b4d9679994
Nikow <nicolas.ruche.entreprise@gmail.com>
2020-09-28 22:42:09 +0200

    Begin to create Random Behaviors. Reorganize Scene in prefabs so that we can work independently

---
+++ Assets/Scripts/Player/Behaviors/Random/PlayerRandomBoatSpawner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRandomBoatSpawner : PlayerBoatSpawner
+{
+    protected override bool ShouldSpawn()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Player/Behaviors/Random/PlayerRandomShooter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRandomShooter : PlayerShoot
+{
+    protected override float AimDirection()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override bool ShouldShoot()
+    {
+        throw new System.NotImplementedException();
+    }
+}
