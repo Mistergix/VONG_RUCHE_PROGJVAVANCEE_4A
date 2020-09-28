0ea07a17a2e868bc0d9db9536565ca57b8eda662
Nikow <nicolas.ruche.entreprise@gmail.com>
2020-09-28 22:31:36 +0200

    Add boat spawning

---
+++ Assets/Scripts/Player/Behaviors/Input/PlayerInputBoatSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputBoatSpawner : PlayerBoatSpawner
+{
+    [SerializeField]
+    private KeyCode spawnKey;
+    protected override bool ShouldSpawn()
+    {
+        return Input.GetKeyDown(spawnKey);
+    }
+}
---
+++ Assets/Scripts/Player/Behaviors/PlayerBoatSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerBoatSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float spawnCoolDown;
+    [SerializeField]
+    private PoolManager boatPool;
+    [SerializeField]
+    private Transform boatSpawn;
+
+    private bool canSpawn;
+
+    private void Start()
+    {
+        Init();
+        canSpawn = true;
+    }
+
+    protected virtual void Init()
+    {
+        
+    }
+
+    protected abstract bool ShouldSpawn();
+
+    internal void HandleBoatSpawn()
+    {
+        if (!canSpawn)
+        {
+            return;
+        }
+
+        if (!ShouldSpawn())
+        {
+            return;
+        }
+
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        canSpawn = false;
+
+        InstantiateBoat();
+
+        yield return new WaitForSeconds(spawnCoolDown);
+
+        canSpawn = true;
+    }
+
+    private void InstantiateBoat()
+    {
+        GameObject boat = boatPool.RequestACopy();
+        boat.transform.position = boatSpawn.position;
+    }
+}
