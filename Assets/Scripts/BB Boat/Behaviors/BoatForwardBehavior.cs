12a204afb014517fb035c47cd6b78e4cea56c020
MaximeVg <Maxime.vong@gmail.com>
2020-09-29 11:34:29 +0200

    Boat forward behaviour

--- Assets/Scripts/BB Boat/Behaviors/BoatForwardBehavior.cs
+++ Assets/Scripts/BB Boat/Behaviors/BoatForwardBehavior.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BoatForwardBehavior : BoatBehavior
 {
+    [SerializeField]
+    private float speed;
+    private Boat boat;
+
+    private void Start() {
+        boat = GetComponent<Boat>();
+    }
     /// <summary>
     /// Le bateau avance à une certaine vitesse (attention à la direction initiale (gauche ou droite))
     /// </summary>
     public override void Execute()
     {
-        Debug.Log("PAS IMPLEMENTE : J'AVANCE");
+        transform.position += boat.Direction() * speed * Time.deltaTime;
     }
 }
