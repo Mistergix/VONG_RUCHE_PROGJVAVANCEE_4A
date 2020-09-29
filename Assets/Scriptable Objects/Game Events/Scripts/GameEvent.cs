69c4fd0d0138869fd65a6362ccc3840a6df55d26
Nikow <nicolas.ruche.entreprise@gmail.com>
2020-09-29 10:16:43 +0200

    just add some events scripts

---
+++ Assets/Scriptable Objects/Game Events/Scripts/GameEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New GameEvent", menuName = "Game Event", order = 51)]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void Raise()
+    {
+        foreach (var listener in listeners)
+        {
+            listener.OnEventRaised();
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        listeners.Remove(listener);
+    }
+}
---
+++ Assets/Scriptable Objects/Game Events/Scripts/GameEventListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    [SerializeField]
+    private GameEvent eventListened;
+    [SerializeField]
+    private UnityEvent response;
+    public GameEvent EventListened { get => eventListened; private set => eventListened = value; }
+
+    private void OnEnable()
+    {
+        EventListened.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        EventListened.UnregisterListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        response.Invoke();
+    }
+}
