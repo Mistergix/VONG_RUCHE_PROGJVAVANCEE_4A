c01b5359b669dc2747124d3524f96261ee1687c3
Nikow <nicolas.ruche.entreprise@gmail.com>
2020-09-29 10:53:51 +0200

    tmp

---
+++ Assets/Scripts/SceneLoading.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoading : MonoBehaviour
+{
+    [SerializeField]
+    private string menuScene = "Menu";
+    [SerializeField]
+    private string gameScene = "Main";
+    [SerializeField]
+    private string gameOverScene = "Game Over";
+
+    public void LaunchGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(menuScene);
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(gameOverScene);
+    }
+}
