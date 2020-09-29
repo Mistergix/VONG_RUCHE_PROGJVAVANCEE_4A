ce992246926489c29ef0c1387f811e059913451a
Nikow <nicolas.ruche.entreprise@gmail.com>
2020-09-29 10:52:13 +0200

    Add initialization data and menu

---
+++ Assets/Scriptable Objects/Initialization Data/InitializationData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Initialization Data", menuName = "Initialization Data", order = 51)]
+public class InitializationData : ScriptableObject
+{
+    public enum AgentType { PLAYER, RANDOM, IA }
+
+    private AgentType leftAgentType, rightAgentType;
+
+    public void SetLeftAgentType(int index)
+    {
+        leftAgentType = (AgentType)index;
+    }
+
+    public void SetRightAgentType(int index)
+    {
+        rightAgentType = (AgentType)index;
+    }
+}
