c19e9e1e052ea208f75c85b0e957e78739a3bc47
Nikow <nicolas.ruche.entreprise@gmail.com>
2020-09-29 11:17:57 +0200

    Working initializing button in the menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Dropdown leftDD, rightDD;
+
+    [SerializeField]
+    private SceneLoading sceneLoading;
+
+    [SerializeField]
+    private InitializationData data;
+
+    public void LaunchGame()
+    {
+        data.SetLeftAgentType(leftDD.value);
+        data.SetRightAgentType(rightDD.value);
+        sceneLoading.LaunchGame();
+    }
+}
