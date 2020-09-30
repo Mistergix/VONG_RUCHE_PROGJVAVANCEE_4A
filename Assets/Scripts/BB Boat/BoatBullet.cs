483d53c370bd3258db4757cec9da6bcca302754c
MaximeVg <Maxime.vong@gmail.com>
2020-09-30 18:04:34 +0200

    Shoot behaviour for BB Boat

---
+++ Assets/Scripts/BB Boat/BoatBullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatBullet : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Vector3 direction;
+
+    [SerializeField]
+    private LayerMask boatMask, playerMask, wallMask;
+
+    public void Init(Vector3 direction) {
+        this.direction = direction; 
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if ((playerMask.value & 1 << collision.gameObject.layer) > 0) {
+            collision.gameObject.GetComponent<Player>().TakeDamage(1);
+            PoolManager.RecycleGameObject(gameObject);
+        } else if ((boatMask.value & 1 << collision.gameObject.layer) > 0) {
+            PoolManager.RecycleGameObject(gameObject);
+            PoolManager.RecycleGameObject(collision.gameObject);
+        } else if ((wallMask.value & 1 << collision.gameObject.layer) > 0) {
+            PoolManager.RecycleGameObject(gameObject);
+        }
+    }
+}
