f9331ecf3ba4ab8c74874645d1b4dcf29873821a
MaximeVg <Maxime.vong@gmail.com>
2020-09-30 12:06:11 +0200

    Dodge Behaviour

--- Assets/Scripts/BB Boat/Behaviors/BoatDodgeBehavior.cs
+++ Assets/Scripts/BB Boat/Behaviors/BoatDodgeBehavior.cs
@@ -2,13 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoatDodgeBehavior : BoatBehavior
-{
+public class BoatDodgeBehavior : BoatBehavior {
+    private Boat boat;
+    private ZoneDetection zoneDetection;
+
+    private void Start() {
+        boat = GetComponent<Boat>();
+        if (boat.Direction().Equals(Vector3.left)) {
+            float pos = zoneDetection.transform.localPosition.x;
+            zoneDetection.transform.localPosition = new Vector3(-pos, transform.position.y, 0f);
+        }
+        zoneDetection.Init();
+    }
+
     /// <summary>
     /// UNE FOIS SI suffisament proche du joueur, se décale un peu pour essayer de le toucher
     /// </summary>
-    public override void Execute()
-    {
-        Debug.Log("PAS IMPLEMENTE : J'ESQUIVE");
+    public override void Execute() {
+
+        if (Vector3.Distance(transform.position, zoneDetection.PlayerPosition) >= 3.5f) {
+            if (zoneDetection.DodgeOnce) {
+                if ((boat.Direction().Equals(Vector3.left) && transform.position.x > zoneDetection.PlayerPosition.x) ||
+                    (boat.Direction().Equals(Vector3.right) && transform.position.x < zoneDetection.PlayerPosition.x)) {
+                    if (transform.position.z > zoneDetection.PlayerPosition.z || transform.position.z < zoneDetection.PlayerPosition.z) {
+                        transform.position =
+                            Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y, transform.position.z),
+                            new Vector3(transform.position.x, transform.position.y, zoneDetection.PlayerPosition.z),
+                            5f * Time.deltaTime);
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnDisable() {
+        zoneDetection = GetComponentInChildren<ZoneDetection>();
+        Reset();
+    }
+    public void Reset() {
+        zoneDetection.Init();
     }
 }
